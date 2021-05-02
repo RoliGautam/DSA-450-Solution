ad2056b06c54f07bb0b6c46542ac06382b21445c
Ankur rai <rai.ankur0007@gmail.com>
2021-05-02 23:43:11 +0530

    added a O(n) time complexity solution

--- array_02.cpp
+++ array_02.cpp
@@ -1,4 +1,3 @@
-#include<iostream>
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -25,5 +24,30 @@ int main(){
        cout<<max;
      
     return 0;
+    
+}
+// added another solution with time complexity O(n) the sort function in above will cost O(nlogn)
+
+int main(){
+   int sz;
+    //we are taking the size of array from the user..
+    cout<<"enter the size of array"<<endl;
+    cin >> sz;
+    int arr[sz]; // some old linters will show error but its ok
+    
+     //this is for scanning the elements of array...
+    cout<<"enter the elements of array"<<endl;
+    for(int i = 0; i<sz; i++){
+    cin>>arr[i];
+    }
+   // we are assuming array will have atleast 1 element other wise we need to put a if statement.
+   int maxEle = arr[0], minEle = arr[0];
+   for(int i = 0; i < sz; i++){
+      maxEle = max(maxEle, arr[i]);
+      minEle = min(minEle, arr[i]);
+   }
+   cout<<"the minimum element is:"<<"\t" << minEle << endl;
+   cout<<"the maximum element is:"<<"\t"<< maxEle << endl;
+   return 0;
     
 }
