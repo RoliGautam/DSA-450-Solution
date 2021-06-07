95f2b96916bf18412aa9e801c68ea105741c9a91
Roli Gautam <72242893+RoliGautam@users.noreply.github.com>
2021-06-07 21:19:35 +0530

    Create KthSmallest.cpp

---
+++ KthSmallest.cpp
@@ -0,0 +1,14 @@
+class Solution{
+    public:
+    // arr : given array
+    // l : starting index of the array i.e 0
+    // r : ending index of the array i.e size-1
+    // k : find kth smallest element and return using this function
+    int kthSmallest(int arr[], int l, int r, int k) {
+        //code here
+        sort(arr,arr+(r+1));
+        int res = arr[k-1];
+        return res;
+        
+    }
+};
